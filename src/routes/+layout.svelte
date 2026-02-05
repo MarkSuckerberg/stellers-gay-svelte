c580daf8ff3d25b38541d821e7d9fd16a08da782
Mark Suckerberg <mark@suckerberg.gay>
2026-02-05 10:13:35 -0600

    all the stuff from who knows how long ago

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,204 @@
 <script lang="ts">
 	import '../app.css';
+	import jary from '$lib/assets/bluejarybeast.webp?enhanced';
+	import construction from '$lib/assets/construction.webp';
+
+	import quoteFile from '$lib/assets/quotes.json';
+	const quotes = quoteFile.quotes;
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="gridcontainer" style:image-rendering="auto">
+	<header id="header">
+		<h1 class="text-3xl font-bold">Steller's Gay</h1>
+	</header>
+
+	<aside id="sidehead">
+		<enhanced:img
+			src={jary}
+			alt="A large raptorlike bluejay-patterned creature laying down, asleep."
+		/>
+	</aside>
+
+	<nav id="nav" class="navcontainer topcontainer">
+		<a href="/" accesskey="h"><u>H</u>ome</a>
+		<a href="/projects" accesskey="p"><u>P</u>rojects</a>
+		<a href="/media" accesskey="m"><u>M</u>edia</a>
+	</nav>
+
+	<nav id="sidebar" class="navcontainer">
+		<a
+			target="_blank"
+			rel="me"
+			href="https://bsky.app/profile/marksuckerberg.bsky.social"
+			accesskey="b"
+			title="I don't use this much!">My <u>B</u>luesky</a
+		>
+		<a
+			target="_blank"
+			rel="me nofollow"
+			href="https://discord.com/users/525509257102098442"
+			accesskey="d">My <u>D</u>iscord</a
+		>
+		<a
+			target="_blank"
+			rel="me"
+			href="https://social.stellers.gay/"
+			accesskey="f"
+			title="I just think it's neat to have!">My <u>F</u>ediverse Instance</a
+		>
+		<a
+			target="_blank"
+			rel="me"
+			href="https://github.com/marksuckerberg"
+			accesskey="g"
+			title="My favourite social media.">My <u>G</u>ithub</a
+		>
+		<a
+			target="_blank"
+			rel="me"
+			href="https://tumblr.suckerberg.gay"
+			accesskey="t"
+			title="My most-used platform.">My <u>T</u>umblog</a
+		>
+
+		<a
+			target="_blank"
+			href="https://hits.stellers.gay/"
+			class="black-background"
+			title="Watch the hits roll in!"
+			><img
+				src="https://hits.stellers.gay/unique/ticker.webp"
+				class="hit-counter"
+				alt="A hitcounter in the style of a seven-segment display."
+			/>
+		</a>
+
+		<a href="mailto:mark@stellers.gay" accesskey="e" title="No spam, plz!"><u>E</u>-mail me!</a>
+		<a
+			target="_blank"
+			rel="me"
+			href="https://ko-fi.com/marksuckerberg"
+			accesskey="k"
+			title="EXTREMELY appreciated!">Buy me a <u>K</u>o-Fi!</a
+		>
+	</nav>
+
+	<main id="content">
+		<p id="construction" class="bg-orange-400">
+			<img
+				src={construction}
+				class="inline-block"
+				alt="An animated diamond-shaped construction sign with a stick figure worker shoveling dirt."
+			/>
+			Warning! This page is under construction. Be sure to check back soon!
+		</p>
+
+		{@render children()}
+	</main>
+
+	<footer id="footer">
+		<noscript>No javascript, no funny quote!</noscript>
+		{quotes[Math.floor(Math.random() * quotes.length)]}
+	</footer>
+</div>
+
+<style>
+	.gridcontainer {
+		display: grid;
+		background-color: #c0c0c0;
+		padding: 10px;
+
+		max-width: 1000px;
+		margin: 0 auto;
+
+		gap: 10px;
+
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-areas:
+			'header header header sidehead'
+			'nav nav nav nav'
+			'sidebar content content content'
+			'footer footer footer footer';
+	}
+
+	.gridcontainer > * {
+		text-align: center;
+		border: black 4px ridge;
+		border-top: white 4px ridge;
+		border-left: white 4px ridge;
+	}
+
+	.gridcontainer #header {
+		grid-area: header;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.gridcontainer #sidehead {
+		grid-area: sidehead;
+		border: white 4px groove;
+		border-top: black 4px groove;
+		border-left: black 4px groove;
+		background-color: #b0b0b0;
+	}
+
+	.gridcontainer #nav {
+		grid-area: nav;
+	}
+
+	.navcontainer {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		border: none;
+	}
+
+	.navcontainer a {
+		border: black 4px ridge;
+		border-top: white 4px ridge;
+		border-left: white 4px ridge;
+		line-height: 200%;
+		text-decoration: none;
+		color: unset;
+	}
+
+	.navcontainer a:active {
+		border: white 4px groove;
+		border-top: black 4px groove;
+		border-left: black 4px groove;
+		background-color: #b0b0b0;
+		text-decoration: none;
+	}
+
+	.topcontainer {
+		flex-direction: row;
+		justify-content: space-around;
+	}
+
+	.topcontainer a {
+		flex-grow: 1;
+	}
+
+	.gridcontainer #sidebar {
+		grid-area: sidebar;
+	}
+
+	.gridcontainer #content {
+		grid-area: content;
+		text-align: left;
+		padding: 5px;
+		overflow-y: auto;
+	}
+
+	.gridcontainer #content img {
+		max-width: 100%;
+		image-rendering: auto;
+	}
+
+	.gridcontainer #footer {
+		grid-area: footer;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,113 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import pixelSafari from '$lib/assets/pixelsafari.webp';
+	import linux from '$lib/assets/1linux2.webp';
+	import shiptest from '$lib/assets/shiptest-button.webp';
+	import css from '$lib/assets/css.webp';
+	import yogstation from '$lib/assets/yogstation.gif';
+	import htmx from '$lib/assets/HTMXstamp.webp';
+	import mitLicense from '$lib/assets/mit-license.webp';
+	import barbedWire from '$lib/assets/barbedwire.webp';
+	import pokeOverworld from '$lib/assets/pokeoverworld.gif';
+	import iLoveCrows from '$lib/assets/ilovecrows.webp';
+</script>
+
+<article>
+	<section class="prose">
+		<h2>Welcome to Steller's Gay!</h2>
+
+		<hr class="rainbow-divider not-prose" />
+
+		<p>
+			My name is Mark Suckerberg, and I'm simply a coder who loves to do too many things at once. I
+			work with many languages, such as C#, Dream Maker, JS, MySQL, PHP, and Python. I don't usually
+			do any kind of contract/paid work unless it interests me personally. If you've got a good idea
+			that you want done, feel free to float it by me by contacting me on Discord (@marksuckerberg)!
+		</p>
+	</section>
+
+	<hr class="paperclip-divider" />
+
+	<p class="center *:inline-block">
+		<a target="_blank" href="https://pixelsafari.neocities.org" title="Awesome site!"
+			><img
+				src={pixelSafari}
+				class="animated-button"
+				alt="An animated button for the Pixel Safari, showing off various graphics they provide, such as Favicons and Dividers."
+			/></a
+		>
+
+		<a target="_blank" href="https://archlinux.org" title="I use Arch Linux btw"
+			><img
+				src={linux}
+				class="animated-button"
+				alt="An animated button showing the windows logo being replaced with Tux the penguin, with the words, 'LinuX Now! 2.0' on the right."
+			/></a
+		>
+
+		<a target="_blank" href="https://shiptest.net" title="Play Shiptest!"
+			><img
+				src={shiptest}
+				class="animated-button"
+				alt="A pixel-art button showing the shiptest logo (sillouhette of a ship in front of a planet eclipsing a star) next to the words 'Shiptest 13' and the BYOND logo."
+			/></a
+		>
+
+		<a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS" title="Learn CSS!"
+			><img
+				src={css}
+				class="animated-button"
+				alt="Button that says 'Made with Cascading Style Sheets'"
+			/></a
+		>
+
+		<a target="_blank" href="https://yogstation.net" title="YOGGERZ!!!"
+			><img
+				src={yogstation}
+				class="animated-button"
+				alt="A pixel-art button showing the Yogstation 13 logo."
+			/></a
+		>
+
+		<a target="_blank" href="https://htmx.org" title="This site was PREVIOUSLY made with HTMX."
+			><img
+				src={htmx}
+				class="animated-button"
+				alt="The logo for the HTMX project, with text saying HTMX Now! 2.0 on the right side"
+			/></a
+		>
+
+		<a
+			target="_blank"
+			rel="license"
+			href="license.txt"
+			title="This site's HTML and CSS is licensed under the MIT license."
+			><img
+				src={mitLicense}
+				class="animated-button"
+				alt="Button that says 'Some Rights Reserved,' along with the MIT logo to represent the MIT License."
+			/></a
+		>
+	</p>
+
+	<p class="center">
+		<img
+			src={barbedWire}
+			loading="lazy"
+			decoding="async"
+			class="stamp"
+			alt="A yellow warning stamp that reads, 'WARNING, MY DIET CONSISTS OF Barbed wire'"
+		/>
+		<img
+			src={pokeOverworld}
+			loading="lazy"
+			decoding="async"
+			class="stamp"
+			alt="An animated stamp of the overworld from Pokemon Heartgold with the male trainer patting their Cyndaquil."
+		/>
+		<img
+			src={iLoveCrows}
+			class="stamp"
+			alt="A stamp that has the sillouhette of a crow on the left, and a heart behind the word crows on the right."
+		/>
+	</p>
+</article>
